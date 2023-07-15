8a0f7eb68d4b923e00faf0b373a83c339ee99271
loipoi3 <qwedsazxc8250@gmail.com>
2023-07-15 15:38:39 +0300

    inference and ui for videos and images

--- inference.py
+++ inference.py
@@ -4,11 +4,48 @@ from ultralytics import YOLO
 from PIL import Image
 import cv2
 import os
+import numpy as np
+import io
 
 app = Flask(__name__)
 
 model = YOLO('./runs/detect/train2/weights/best.pt')
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+
+def visualize_detection(image, detections):
+    image = np.array(image)
+    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+    class_names = ['10c', '10d', '10h', '10s', '2c', '2d', '2h', '2s', '3c', '3d', '3h', '3s', '4c', '4d', '4h', '4s',
+                   '5c', '5d', '5h', '5s', '6c', '6d', '6h', '6s', '7c', '7d', '7h', '7s', '8c', '8d', '8h', '8s', '9c',
+                   '9d', '9h', '9s', 'Ac', 'Ad', 'Ah', 'As', 'Jc', 'Jd', 'Jh', 'Js', 'Kc', 'Kd', 'Kh', 'Ks', 'Qc', 'Qd',
+                   'Qh', 'Qs']
+
+    for bbox in detections:
+        x_min, y_min, x_max, y_max, prob_class, class_id = bbox
+        class_id = int(class_id)
+        label = f"{class_names[class_id]}: {prob_class:.2f}"
+        color = (0, 0, 255)
+        thickness = 1
+        font_size = 0.3
+        font_thickness = 1
+        text_offset = 2
+
+        text_size, _ = cv2.getTextSize(label, cv2.FONT_HERSHEY_SIMPLEX, font_size, font_thickness)
+        cv2.rectangle(image, (int(x_min), int(y_min - text_size[1] - text_offset)),
+                      (int(x_min) + text_size[0], int(y_min)), (0, 0, 255), cv2.FILLED)
+
+        # Draw white text above the bounding box
+        cv2.putText(image, label, (int(x_min), int(y_min - text_offset)), cv2.FONT_HERSHEY_SIMPLEX, font_size,
+                    (255, 255, 255), font_thickness)
+
+        # Draw the bounding box
+        cv2.rectangle(image, (int(x_min), int(y_min)), (int(x_max), int(y_max)), color, thickness)
+
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    image = Image.fromarray(image)
+    return image
+
 
 @app.route('/detect_images', methods=['POST'])
 def run_inference_img():
@@ -19,11 +56,13 @@ def run_inference_img():
     image = Image.open(file.stream)
 
     results = model(image)
-    serialized_results = {
-        'data': results[0].boxes.data.tolist()
-    }
 
-    return jsonify(serialized_results)
+    img = visualize_detection(image, results[0].boxes.data.tolist())
+    img_byte_arr = io.BytesIO()
+    img.save(img_byte_arr, format='JPEG')
+    img_byte_arr = img_byte_arr.getvalue()
+
+    return img_byte_arr, 200, {'Content-Type': 'image/jpeg'}
 
 
 @app.route('/detect_videos', methods=['POST'])
@@ -48,7 +87,7 @@ def run_inference_video():
     fps = cap.get(cv2.CAP_PROP_FPS)
 
     # Create a video writer for the output video
-    fourcc = cv2.VideoWriter_fourcc(*'XVID')
+    fourcc = cv2.VideoWriter_fourcc(*'avc1')
     out = cv2.VideoWriter(output_path, fourcc, fps, (frame_width, frame_height))
 
     while cap.isOpened():
@@ -65,20 +104,25 @@ def run_inference_video():
         # Get the bounding boxes from the results
         bboxes = results[0].boxes.data.tolist()
 
+        class_names = ['10c', '10d', '10h', '10s', '2c', '2d', '2h', '2s', '3c', '3d', '3h', '3s', '4c', '4d', '4h',
+                       '4s', '5c', '5d', '5h', '5s', '6c', '6d', '6h', '6s', '7c', '7d', '7h', '7s', '8c', '8d', '8h',
+                       '8s', '9c', '9d', '9h', '9s', 'Ac', 'Ad', 'Ah', 'As', 'Jc', 'Jd', 'Jh', 'Js', 'Kc', 'Kd', 'Kh',
+                       'Ks', 'Qc', 'Qd', 'Qh', 'Qs']
+
         # Draw bounding boxes on the frame
         for bbox in bboxes:
             x_min, y_min, x_max, y_max, prob_class, class_id = bbox
             color = (0, 0, 255)
             thickness = 2
             cv2.rectangle(frame, (int(x_min), int(y_min)), (int(x_max), int(y_max)), color, thickness)
-            label = f"{class_id}: {prob_class:.2f}"
+            label = f"{class_names[int(class_id)]}: {prob_class:.2f}"
             font = cv2.FONT_HERSHEY_SIMPLEX
-            font_scale = 0.5
-            font_thickness = 1
+            font_scale = 1.3
+            font_thickness = 2
             text_size, _ = cv2.getTextSize(label, font, font_scale, font_thickness)
             cv2.rectangle(frame, (int(x_min), int(y_min) - text_size[1]),
                           (int(x_min) + text_size[0], int(y_min)), color, cv2.FILLED)
-            cv2.putText(frame, label, (int(x_min), int(y_min)), font, font_scale, (0, 0, 0), font_thickness)
+            cv2.putText(frame, label, (int(x_min), int(y_min)), font, font_scale, (255, 255, 255), font_thickness)
 
         # Write the frame with bounding boxes to the output video
         out.write(frame)
--- ui.py
+++ ui.py
@@ -2,8 +2,7 @@ import streamlit as st
 import requests
 from PIL import Image
 import io
-import cv2
-import numpy as np
+import shutil
 
 
 def send_prediction_request_img(image):
@@ -15,37 +14,9 @@ def send_prediction_request_img(image):
     try:
         response = requests.post(url, files=files)  # Pass the headers argument
         response.raise_for_status()
-        result = response.json()
-        return result.get('data')
+        return response.content
     except requests.exceptions.RequestException as e:
         return f'Error: {e}'
-
-
-def visualize_detection(image, detections):
-    image = np.array(image)
-    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
-    class_names = ['10c', '10d', '10h', '10s', '2c', '2d', '2h', '2s', '3c', '3d', '3h', '3s', '4c', '4d', '4h', '4s',
-                   '5c', '5d', '5h', '5s', '6c', '6d', '6h', '6s', '7c', '7d', '7h', '7s', '8c', '8d', '8h', '8s', '9c',
-                   '9d', '9h', '9s', 'Ac', 'Ad', 'Ah', 'As', 'Jc', 'Jd', 'Jh', 'Js', 'Kc', 'Kd', 'Kh', 'Ks', 'Qc', 'Qd',
-                   'Qh', 'Qs']
-
-    for bbox in detections:
-        x_min, y_min, x_max, y_max, prob_class, class_id = bbox
-        class_id = int(class_id)
-        label = f"{class_names[class_id]}: {prob_class:.2f}"
-        color = (0, 0, 255)
-        thickness = 1
-        font_size = 0.25
-        font_thickness = 1
-        text_offset = 2
-
-        cv2.rectangle(image, (int(x_min), int(y_min)), (int(x_max), int(y_max)), color, thickness)
-        cv2.putText(image, label, (int(x_min), int(y_min - text_offset)), cv2.FONT_HERSHEY_SIMPLEX, font_size, color,
-                    font_thickness, cv2.LINE_AA, False)
-
-    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-    image = Image.fromarray(image)
-    st.image(image, caption='Object Detection', use_column_width=True)
 
 
 def main():
@@ -59,7 +30,7 @@ def main():
             if isinstance(res, str) and res.startswith('Error'):
                 st.error(res)
             else:
-                visualize_detection(image, res)
+                st.image(res, caption='Object Detection', use_column_width=True)
         elif uploaded_file.type.startswith('video'):
             video = uploaded_file.getvalue()
             v = st.video(video)
@@ -73,9 +44,9 @@ def main():
 
                     # Display the output video in Streamlit
                     st.video(output_video)
+                    shutil.rmtree('./temp')
                 else:
                     st.error(f"Error: {predicted.status_code} - {predicted.content}")
-
 
 if __name__ == '__main__':
     st.set_page_config(page_title="Object Detection")
